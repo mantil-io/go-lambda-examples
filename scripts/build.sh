d99e1436df7d14e0108745b1986424d5419ce00e
Igor Anic <ianic@inet.hr>
2022-01-29 10:47:30 +0100

    allow setting payload in invoke script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash -e
 
 echo "=> build"
-GOOS=linux GOARCH=arm64 go build -o bootstrap
+GOOS=linux GOARCH=arm64 go build -trimpath -o bootstrap
 
 echo "=> create deployment package"
 zip function.zip bootstrap $@
--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash -e
 
 function_name="${1:-go-handler-example}"
+payload=${2:-'{"name":"Foo"}'}
 
 aws lambda invoke \
   --function-name "$function_name" \
   --no-cli-pager \
   --cli-binary-format raw-in-base64-out \
-  --payload '{"name":"Foo"}' \
+  --payload "${payload}" \
   response.json && cat response.json
 
 rm response.json
