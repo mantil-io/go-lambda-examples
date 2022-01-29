d3578fe73dd15cc3d274d82e9f7312b8d5e3cdb5
Igor Anic <ianic@inet.hr>
2022-01-29 10:48:41 +0100

    add waiting for SIGTERM into handler

--- handler/main.go
+++ handler/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
 type MyEvent struct {
@@ -12,9 +18,26 @@ type MyEvent struct {
 }
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	if lc, ok := lambdacontext.FromContext(ctx); ok {
+		log.Printf("requestID: %s", lc.AwsRequestID)
+	}
+	for _, e := range os.Environ() {
+		log.Printf("%s", e)
+	}
+
 	return fmt.Sprintf("Hello %s!", name.Name), nil
 }
 
 func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		log.Printf("waiting for the SIGTERM ")
+		s := <-sigs
+		log.Printf("received signal %s", s)
+		time.Sleep(500 * time.Millisecond)
+		log.Printf("done")
+	}()
+
 	lambda.Start(HandleRequest)
 }
